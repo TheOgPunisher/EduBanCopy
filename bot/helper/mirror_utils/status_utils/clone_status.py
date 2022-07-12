bf962c24333dee027e8ddc7413a39621e55b9d9e
TheOgPunisher <101314367+TheOgPunisher@users.noreply.github.com>
2022-07-12 14:05:08 +0530

    Update clone_status.py

--- bot/helper/mirror_utils/status_utils/clone_status.py
+++ bot/helper/mirror_utils/status_utils/clone_status.py
@@ -1,4 +1,4 @@
-from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time
+from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time, EngineStatus
 
 
 class CloneStatus:
